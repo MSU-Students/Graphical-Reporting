4b11cdb83caafcdd4d0e19ef4d4fec1bc77965e0
RohmaCarim <rohmacarim11@gmail.com>
2021-08-23 16:56:02 +0800

    initial commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <div class="text-h5 text-center text-bold q-pt-x1 q-mt-x1"> Login to continue </div>
+    <div class="q-pa-sm"></div>
+    <q-input v-model="email" label="Email" outline></q-input>
+    <q-input v-model="password" label="Password" outline ="qmt-sm"></q-input>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/landingpage.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>hgfhgfg</h1>
+</template>
