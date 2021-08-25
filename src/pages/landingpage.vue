cd848748993b22ba50eedf2b5960b9c712031a1d
RohmaCarim <rohmacarim11@gmail.com>
2021-08-25 16:28:59 +0800

    landing view

--- src/pages/landingpage.vue
+++ src/pages/landingpage.vue
@@ -1,8 +1,15 @@
 <template>
 <q-page class = "bg-image">
 
- <div class="text-center text-h4 text-bold text-arial text-brown">
-            L O G I N
+ <div class="text-center text-h3 text-bold text-arial text-brown q-pt-lg">
+            Welcome To The MSU Student Management System with Data Driven Graphical Reporting
+          </div>
+
+          <div class="text-center text-h6  text-arial text-brown q-pa-lg">
+            This project will show a graphical report of General Enrollment Statistic,
+             General Graduate Statistic, General Grade Statistic, General GPA Statistic,
+              General CGPA Statistic, General Dropout Statistic, General INC Statistic,
+             General Failing Grade Statistic, demographical, gender, course, address.
           </div>
   </q-page>
 
