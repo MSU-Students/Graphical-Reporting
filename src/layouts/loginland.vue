941ac4b446101458b08a58b5baa7d5d60554f5b8
RohmaCarim <rohmacarim11@gmail.com>
2021-08-25 16:22:19 +0800

    text in landing

--- src/layouts/loginland.vue
+++ src/layouts/loginland.vue
@@ -2,23 +2,7 @@
   <q-layout view="lHh Lpr lFf ">
 
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
 
-        <q-toolbar-title>
-          Quasar Apps
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-
-      </q-toolbar>
     </q-header>
     <q-page-container>
       <router-view />
--- src/pages/landingpage.vue
+++ src/pages/landingpage.vue
@@ -1,6 +1,9 @@
 <template>
 <q-page class = "bg-image">
 
+ <div class="text-center text-h4 text-bold text-arial text-brown">
+            L O G I N
+          </div>
   </q-page>
 
 </template>
