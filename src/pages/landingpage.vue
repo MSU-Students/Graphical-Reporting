06a601e5c77d7221829f2f39b143c3a1e428b858
RohmaCarim <rohmacarim11@gmail.com>
2021-08-25 16:50:10 +0800

    button to landing

--- src/pages/landingpage.vue
+++ src/pages/landingpage.vue
@@ -11,6 +11,29 @@
               General CGPA Statistic, General Dropout Statistic, General INC Statistic,
              General Failing Grade Statistic, demographical, gender, course, address.
           </div>
+          <div>
+      <div class="q-pa-md q-gutter-md flex flex-center">
+      <q-btn-toggle
+        v-model="model"
+        toggle-color="primary"
+        to="Login"
+
+        flat
+        :options="[
+          {label: 'Read More'},
+        ]"
+      />
+      </div>
+    </div>
   </q-page>
 
 </template>
+<script>
+export default {
+  data () {
+    return {
+      model: ''
+    }
+  }
+}
+</script>
