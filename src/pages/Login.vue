878b78ebf05315bb14c40f8d214020dd5990450a
RohmaCarim <rohmacarim11@gmail.com>
2021-08-26 21:16:39 +0800

    remove rgb

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,43 +1,38 @@
 <template>
   <q-page class="bg-image">
-    <div class="q-pa-md flex flex-center">
-      <div class="q-gutter-s">
-
-          <div>
-            <div></div>
-            <div class="text-center text-h4 text-bold text-italic">
-              L O G I N
+    <div class="flex flex-center absolute-center">
+      <div class="q-pa-xxl">
+        <div>
+          <div class="text-center text-h4 text-bold text-italic">L O G I N</div>
+          <div class="q-pa-md" style="width: 300px">
+            <q-input filled v-model="text" label="Email" />
+            <div class="q-pa-md row items-start" style="width: 300px">
+              <q-input
+                v-model="password"
+                label="password"
+                filled
+                :type="isPwd ? 'password' : 'text'"
+                hint=""
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
             </div>
-            <div class="q-pa-md" style="width: 300px">
-              <q-input filled v-model="text" label="Email" />
-              <div class="q-pa-md row items-start q-pa-md" style="width: 300px">
-                <q-input
-                  v-model="password"
-                  label="password"
-                  filled
-                  :type="isPwd ? 'password' : 'text'"
-                  hint=""
-                >
-                  <template v-slot:append>
-                    <q-icon
-                      :name="isPwd ? 'visibility_off' : 'visibility'"
-                      class="cursor-pointer"
-                      @click="isPwd = !isPwd"
-                    />
-                  </template>
-                </q-input>
-              </div>
 
-              <q-btn
-                outline
-                rounded
-                color="primary"
-                label="login"
-                to="landingpage"
-              />
-            </div>
+            <q-btn
+              outline
+              rounded
+              color="primary"
+              label="login"
+              to="landingpage"
+            />
           </div>
-
+        </div>
       </div>
     </div>
   </q-page>
