0d7033cd82f1361032350bd9263a3d99215a96e9
RohmaCarim <rohmacarim11@gmail.com>
2021-09-10 09:29:11 +0800

    managepage

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,7 +7,7 @@ const routes: RouteRecordRaw[] = [
     children: [
       { path: '', component: () => import('pages/landingpage.vue') },
       { path: '/landingpage', component: () => import('pages/landingpage.vue') },
-      { path: '/Login', component: () => import('pages/Login.vue') },
+      { path: '/ManageStudentPage', component: () => import('pages/ManageStudentPage.vue') },
 
 
     ],
